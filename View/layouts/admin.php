42bf574e6d1875452a3afe0e870771d8a66e6034
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 14:36:39 +0200

    page admin

---
+++ View/layouts/admin.php
@@ -0,0 +1,12 @@
+<section>
+    <div>
+        <h2>Statistique : </h2>
+        <p>Photos posté : <?php ?></p>
+        <p>Nombre de compte : <?php ?></p>
+        <p>Nombre de compte Warn : <?php ?></p>
+    </div>
+    <div>
+        <h2>Les comptes warn : </h2>
+        <!-- afficher les comptes en php -->
+    </div>
+</section>
