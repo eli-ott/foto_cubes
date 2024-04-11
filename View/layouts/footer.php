da4a9b79c05f1c1ea7c7defb83763218ea224f5d
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 11:41:55 +0200

    ajout footer.php

---
+++ View/layouts/footer.php
@@ -0,0 +1,21 @@
+<footer>
+    <p>Lorem ipsum</p>
+    <div class="links">
+        <div>
+            <p class="title">Nous contacter</p>
+            <ul>
+                <li><a href="mailto:example@gmail.com">example@gmail.com</a></li>
+                <li><a href="machign.git">machign.git</a></li>
+            </ul>
+        </div>
+        <div>
+            <p class="title">Liens rapides</p>
+            <ul>
+                <li><a href="./">Accueil</a></li>
+                <li><a href="#">#</a></li>
+                <li><a href="#">#</a></li>
+            </ul>
+        </div>
+    </div>
+    <p class="copyright">Copyright Foto</p>
+</footer>
