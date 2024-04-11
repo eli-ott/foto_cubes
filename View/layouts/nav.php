98697c6a087535350bbe8f19d87930c6c1522d0c
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 11:40:50 +0200

    ajout des fichiers base et nav php ainsi que le logo

---
+++ View/layouts/nav.php
@@ -0,0 +1,10 @@
+<nav>
+    <a class="logo" href="./index.html">
+        <img src="/front/assets/images/LimeWire_AI_Studio_Asset__7_-removebg-preview 1.svg" alt="Logo Foto" />
+        <p>Foto</p>
+    </a>
+    <ul class="menu">
+        <li><a href="#">Galerie</a></li>
+        <li><a href="#">Mon compte</a></li>
+    </ul>
+</nav>
