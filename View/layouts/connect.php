2673dbb397264db7303f5b52948f1d4ab1648d16
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 12:09:01 +0200

    ajout page connexion

---
+++ View/layouts/connect.php
@@ -0,0 +1,11 @@
+<section>
+    <div>
+        <h2>Me connecter</h2>
+        <form action="#" method="post">
+            <input type="text" placeholder="Pseudo">
+            <input type="text" placeholder="Mot de passe">
+            <a href="mdp-oublie">Mot de passe oublier ?</a>
+            <button type="button">Connextion</button>
+        </form>
+    </div>
+</section>
