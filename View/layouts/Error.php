5eb04554fa1b5e2d1b33940f850248e958f3fe9c
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-11 14:15:55 +0200

    fix erreur

--- View/layouts/Error.php
+++ View/layouts/Error.php
@@ -2,5 +2,6 @@
     <div>
         <h2>404</h2>
         <p>Oups, il s’emblerait qu’il y ai une erreur: “error message”</p>
+        <button type="button">Retour à l'accueil</button>
     </div>
 </section>
