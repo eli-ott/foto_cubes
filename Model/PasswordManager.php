66602b19cd73183ba5d267b15e1632c3f550e5c7
baptiste vauthier <ba.vauthier@gmail.com>
2024-04-16 10:52:58 +0200

    crud mdp

---
+++ Model/PasswordManager.php
@@ -0,0 +1,111 @@
+<?php
+require_once('./Model/Model.php');
+
+"udpate - delete - get - create";
+
+class PasswordManager extends Model
+{
+    /**
+     * Update le mot de passe de l'utiliateur
+     * 
+     * @param string $hash Le mot de passe
+     * @param int $nb_essais Référence le nombre de fois que le mot de passe a été changé
+     * @param int $id_user Récupère l'id de l'utiliateur qui souhaite changer son password
+     * @return int Le code status de la requete
+     */
+    public function updatePassword(string $hash, int $nb_essais, int $id_user): int
+    {
+        $sql = "UPDATE mot_de_passe SET `hash` = :mdp, nb_essais = :nb_essais WHERE id_user = :id_user";
+
+        $req = $this->getBDD()->prepare($sql, [
+            "mdp" => $hash,
+            "nb_essais" => $nb_essais,
+            "id_user" => $id_user
+        ]);
+        $req->execute();
+
+        if (!$req) {
+            $status = 500;
+        } else {
+            $status = 200;
+        }
+
+        return $status;
+    }
+
+    /**
+     * Supprime le compte de l'utiliateur
+     * 
+     * @param int $id_user Récupère l'id de l'utiliateur qui souhaite supprimer son compte
+     * @return int Le code status de la requete
+     */
+    public function deletePassword(int $id_user): int
+    {
+        $sql = "DELETE FROM ot_de_passe WHERE id_user = :id_user";
+
+        $req = $this->getBDD()->prepare($sql, [
+            "id_user" => $id_user
+        ]);
+        $req->execute();
+
+        if (!$req) {
+            $status = 500;
+        } else {
+            $status = 200;
+        }
+
+        return $status;
+    }
+
+    /**
+     * Récuperer le password de l'utiliateur
+     * 
+     * @param int $id_user Récupère l'id de l'utilisateur 
+     * @return int Le code status dela requete
+     */
+    public function getPassword(int $id_user): int
+    {
+        $sql = "SELECT mot_de_passe.id_mot_de_passe, `hash`, date_reinitialisation, mot_de_passe.id_user, pseudo FROM mot_de_passe INNER JOIN utilisateur ON mot_de_passe.id_user = utilisateur.id_user WHERE id_user = :id_user";
+
+        $req = $this->getBDD()->prepare($sql, [
+            "id_user" => $id_user
+        ]);
+        $req->execute();
+
+        if (!$req) {
+            $status = 500;
+        } else {
+            $status = 200;
+        }
+
+        return $status;
+    }
+
+    /**
+     * Crée un password pour un nouvelle utilisateur
+     * 
+     * @param int $id_mot_de_passe Crée un id pour le mdp
+     * @param int $id_user Lie le mdp a l'utilisateur
+     * @param string $hash Stock le mdp hasher
+     * @return int Le code status de la requete
+     */
+    public function createPassword(int $id_mot_de_passe, int $id_user, string $hash): int
+    {
+        $sql = "INSERT INTO mot_de_passe ( id_mot_de_passe, id_user, `hash`) VALUE (:id_mdp, :id_user, :mdp)";
+
+        $req = $this->getBDD()->prepare($sql, [
+            "id_mdp" => $id_mot_de_passe,
+            "id_user" => $id_user,
+            "mdp" => $hash
+        ]);
+        $req->execute();
+
+        if (!$req) {
+            $status = 500;
+        } else {
+            $status = 200;
+        }
+
+        return $status;
+    }
+}
