30876b7a042dea16f1d3702466de97bf24d0757c
eli-ott <eliottbidault@gmail.com>
2024-04-16 10:00:30 +0200

    Fix erreur commentaire

--- Scripts/scripts-bdd.sql
+++ Scripts/scripts-bdd.sql
@@ -3,7 +3,7 @@ USE Foto;
 
 CREATE TABLE IF NOT EXISTS `mot_de_passe` (
     id_mot_de_passe INT PRIMARY KEY NOT NULL AUTO_INCREMENT COMMENT 'L''identifiant du mot de passe',
-    id_user INT NOT NULL 'L''identifiant de l''utilisateur',
+    id_user INT NOT NULL COMMENT 'L''identifiant de l''utilisateur',
     `hash` TEXT NOT NULL COMMENT 'Le hash du mot de passe',
     nb_essais INT NOT NULL DEFAULT 0 COMMENT 'Le nombre d''essais de connexion, remis à zéro à chaque connexion réussi',
     date_reinitialisation INT DEFAULT NULL COMMENT 'La dernière date de réinitialisation',
