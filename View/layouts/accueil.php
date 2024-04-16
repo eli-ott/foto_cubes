8680d17dde6d7eb9bf111148b658fd9e158411b9
eli-ott <eliottbidault@gmail.com>
2024-04-16 09:54:56 +0200

    Début du style de la page d'accueil

--- View/layouts/accueil.php
+++ View/layouts/accueil.php
@@ -1,32 +1,40 @@
-    <!-- BANNER -->
-    <div class="banner">
-        <div>
-            <h2 class="title_accueil">Ce site est un site de partage de photos entre amateurs</h2>
-            <button class="cta">Voir Galerie</button>
-            <button>Ajouter photos</button>
+    <section class="accueil">
+        <!-- BANNER -->
+        <div class="banner flex start">
+            <div class="flex center column">
+                <h2 class="title_accueil">Foto, le meilleur site de partage de photo entre amateur</h2>
+                <div class="buttons flex start">
+                    <a href="gallerie" class="cta">Voir la galerie</a>
+                    <a href="ajouter">Ajouter des photos</a>
+                </div>
+            </div>
         </div>
-    </div>
-    <!-- Pourquoi Foto ? -->
-    <h2>Pourquoi Foto ?</h2>
-    <p class="description">
-        Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-        architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
-        voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
-    </p>
-    <!-- Dernière photos poster -->
-    <div class="last_pic">
-        <img src="#" alt="#" />
-        <img src="#" alt="#" />
-        <img src="#" alt="#" />
-        <img src="#" alt="#" />
-        <img src="#" alt="#" />
-        <button class="more">Voir plus</button>
-    </div>
-    <!-- CCM -->
-    <div class="ccm">
-        <p class="description">
-            Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
-            architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni.
-        </p>
-        <h2>Comment ça marche ?</h2>
-    </div>
+        <!-- Pourquoi Foto ? -->
+        <div class="flex center content">
+            <h3>Pourquoi Foto ?</h3>
+            <p class="description">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni dolores eos qui ratione
+                voluptatem sequi nesciunt. Neque porro quisquam est, qui dolorem ipsum quia dolor sit amet.
+            </p>
+        </div>
+        <!-- Dernière photos poster -->
+        <div class="preview flex center column">
+            <div class="pictures flex center">
+                <div class="pic"></div>
+                <div class="pic"></div>
+                <div class="pic"></div>
+                <div class="pic"></div>
+                <div class="pic"></div>
+            </div>
+            <a href="gallerie" class="more">Voir plus</a>
+        </div>
+        <!-- CCM -->
+        <div class="flex center content">
+            <p class="description">
+                Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
+                architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni.
+            </p>
+            <h3>Comment ça marche ?</h3>
+        </div>
+    </section>
