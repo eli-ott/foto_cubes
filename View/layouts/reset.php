605e6168cedda66df504159244bcda348b17461b
eli-ott <eliottbidault@gmail.com>
2024-04-25 17:55:37 +0200

    Création de la page de réinitialisation du mot de passe

---
+++ View/layouts/reset.php
@@ -0,0 +1,12 @@
+<section class="reset flex center">
+    <div class="form-container flex start column">
+        <h2>Réinitialiser mon mot de passe</h2>
+        <form action="#" method="post" class="flex row wrap">
+            <input type="email" placeholder="Email">
+            <input type="password" placeholder="Mot de passe actuel">
+            <input type="password" placeholder="Nouveau mot de passe">
+            <input type="password" placeholder="Confirmer le mot de passe">
+            <button type="button">Réinitialiser</button>
+        </form>
+    </div>
+</section>
