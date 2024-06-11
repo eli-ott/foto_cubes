bb591f75cf89c6c872cd7d8fc3f8180c8181b0b9
baptiste vauthier <ba.vauthier@gmail.com>
2024-06-11 10:33:42 +0200

    fix css accueil mobile

--- View/layouts/accueil.php
+++ View/layouts/accueil.php
@@ -37,7 +37,7 @@
             </div>
         <?php endif; ?>
         <!-- CCM -->
-        <div class="flex center content">
+        <div class="ccm flex center content">
             <p class="description">
                 Sed ut perspiciatis unde omnis iste natus error sit voluptatem accusantium doloremque laudantium, totam rem aperiam, eaque ipsa quae ab illo inventore veritatis et quasi
                 architecto beatae vitae dicta sunt explicabo. Nemo enim ipsam voluptatem quia voluptas sit aspernatur aut odit aut fugit, sed quia consequuntur magni.
